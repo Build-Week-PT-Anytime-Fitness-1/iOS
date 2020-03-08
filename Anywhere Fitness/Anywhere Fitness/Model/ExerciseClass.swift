0f5c78d5acda4f1d668822dd3526ac27f83e477d
Alex Shillingford <alex.shillingford@icloud.com>
2020-03-08 07:57:42 -0600

    fetchAllClasses() now properly decodes documents into ExerciseClass type

--- Anywhere Fitness/Anywhere Fitness/Model/ExerciseClass.swift
+++ Anywhere Fitness/Anywhere Fitness/Model/ExerciseClass.swift
@@ -7,15 +7,65 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import Firebase
 
 struct ExerciseClass {
-    var name, location: String
-    var type: ExerciseType
-    var startTime: Date
-    var duration: String
-    var intensityLevel: IntensityLevel
-    var attendees, maxClassSize: Int // Maybe consider changing to a double since cloud Firestore puts all numbers as doubles
-    var id: UUID
+    var name, location: String?
+    var type: ExerciseType?
+    var startTime: Date?
+    var duration: String?
+    var intensityLevel: IntensityLevel?
+    var attendees, maxClassSize: Double? // Maybe consider changing to a double since cloud Firestore puts all numbers as doubles
+    var id: UUID?
+    
+    var dictionary : [String:Any] {
+         return [
+                 "name": name  ?? "",
+                 "location": location  ?? "",
+                 "type": type  ?? "",
+                 "startTime": startTime  ?? "",
+                 "duration": duration  ?? "",
+                 "intensityLevel": intensityLevel  ?? "",
+                 "attendees": attendees  ?? "",
+                 "maxClassSize": maxClassSize  ?? "",
+                 "id": id  ?? "",
+         ]
+     }
+
+    init(snapshot: QueryDocumentSnapshot) {
+        id = UUID(uuidString: snapshot.documentID)
+        let snapshotValue = snapshot.data()
+         name = snapshotValue["name"] as? String
+         location = snapshotValue["location"] as? String
+        type = snapshotValue["type"] as? ExerciseType
+        startTime = snapshotValue["startTime"] as? Date
+        duration = snapshotValue["duration"] as? String
+        intensityLevel = snapshotValue["intensityLevel"] as? IntensityLevel
+        attendees = snapshotValue["attendees"] as? Double
+        maxClassSize = snapshotValue["maxClassSize"] as? Double
+     }
+    
+    init(name: String,
+         location: String,
+         type: ExerciseType,
+         startTime: Date,
+         duration: String,
+         intensityLevel: IntensityLevel,
+         attendees: Double,
+         maxClassSize: Double,
+         id: UUID = UUID()) {
+        
+        self.name = name
+        self.location = location
+        self.type = type
+        self.startTime = startTime
+        self.duration = duration
+        self.intensityLevel = intensityLevel
+        self.attendees = attendees
+        self.maxClassSize = maxClassSize
+        self.id = id
+    }
 }
 
 enum ExerciseType: String {
